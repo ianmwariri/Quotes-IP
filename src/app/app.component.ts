79d462e846835c13e3de303b7dc45255ad16d110
Ian Mwariri <mwangi.ian3@gmail.com>
2020-08-24 23:36:34 +0300

    Run build for page Deployment.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
   quotes:Quote[]=[
     new Quote(1, 'Believe in yourself, and others will too.','Author: Ian, Submitted by: Mwariri, Votes: 14', new Date(2020,8,20)),
     new Quote(2, 'Man is what he wills himself to be.', 'Author: Albert, Submitted by: Liam, Votes: 9', new Date(2020,7,23)),
-    new Quote(3, 'Liberties arent given, they are taken.', 'Author: Ford, Submitted by: Henry, Votes: 10', new Date(2020, 3,3)),
+    new Quote(3, 'Liberties arent given, they are taken.', 'Author: Ford, Submitted by: Henry, Votes: 12', new Date(2020, 3,3)),
   ];
 
   toggleDetails(index){
