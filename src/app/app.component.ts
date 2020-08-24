963bd4e1ba3b0b62fd8dc1a1a69ea7e61572804f
Ian Mwariri <mwangi.ian3@gmail.com>
2020-08-24 18:32:07 +0300

    Add Date tag to Each Quote Posted.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,9 @@ export class AppComponent {
   title = 'Quotes-IP';
   
   quotes:Quote[]=[
-    new Quote(1, 'You are on Mute.','Author: Ian, Submitted by: Mwariri, Votes: 7'),
-    new Quote(2, 'Man is what he wills himself to be.', 'Author: Albert, Submitted by: Liam, Votes: 700'),
-    new Quote(3, 'Liberties arent given, they are taken.', 'Author: Ford, Submitted by: Henry, Votes: 1000'),
+    new Quote(1, 'You are on Mute.','Author: Ian, Submitted by: Mwariri, Votes: 7', new Date(2020,8,20)),
+    new Quote(2, 'Man is what he wills himself to be.', 'Author: Albert, Submitted by: Liam, Votes: 700', new Date(2020,7,23)),
+    new Quote(3, 'Liberties arent given, they are taken.', 'Author: Ford, Submitted by: Henry, Votes: 1000', new Date(2020, 3,3)),
   ];
 
   toggleDetails(index){
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showQuoteDescription: boolean;
-    constructor(public id: number, public quoteTitle: string, public quoteDescription: string){
+    constructor(public id: number, public quoteTitle: string, public quoteDescription: string, public postedDate: Date){
         this.showQuoteDescription=false;
     }
 }
