09a920ec7000900d335bebce85dad7810267af23
Ian Mwariri <mwangi.ian3@gmail.com>
2020-08-24 11:56:07 +0300

    Add a Goal Detail Component.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,5 @@
 export class Quote {
     id: number;
     quoteTitle: string;
-    // quoteDescription: string;
+    quoteDescription: string;
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,9 +9,9 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
-    {id:1, quoteTitle:'You are on Mute.'},
-    {id:2, quoteTitle:'Man is what he wills himself to be.'},
-    {id:3, quoteTitle:'Liberties arent given, they are taken.'},
+    {id:1, quoteTitle:'You are on Mute.',quoteDescription:'Author: Ian, Submitted by: Mwariri, Votes: 7'},
+    {id:2, quoteTitle:'Man is what he wills himself to be.',quoteDescription:'Author: Albert, Submitted by: Liam, Votes: 700'},
+    {id:3, quoteTitle:'Liberties arent given, they are taken.',quoteDescription:'Author: Ford, Submitted by: Henry, Votes: 1000'},
   ]
   constructor() { }
 
